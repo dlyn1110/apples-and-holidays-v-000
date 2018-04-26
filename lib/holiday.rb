0eea10e069ca67655beb4aa011c9bf26d3a75858
Dani Lyn <dlyn1110@gmail.com>
2018-04-26 18:59:09 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -51,14 +51,8 @@ end.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
-  # iterate through holiday_hash and print items such that your readout resembles:
-  # Winter:
-  #   Christmas: Lights, Wreath
-  #   New Years: Party Hats
-  # Summer:
-  #   Fourth Of July: Fireworks, BBQ
-  # etc.
-holiday_hash.flatten
+  
+  holiday_hash.flatten
 end
 
 def all_holidays_with_bbq(holiday_hash)
